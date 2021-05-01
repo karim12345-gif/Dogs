672e8099ec897f3c2ba33f1e07bae1eb74fbd9b1
karim tariq salim <62877005+karim12345-gif@users.noreply.github.com>
2021-05-01 16:47:30 +0400

    Add files via upload

---
+++ PlayerControllerX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab; // we attach this script to the player , and then we should attach the dog prefab to it
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
